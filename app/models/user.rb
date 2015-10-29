7cf02b150d355e81424d93f1681563506a381f8b
Mikael Lisselgard <mikael.lisselgard@hotmail.se>
2015-10-29 13:42:42 +0000

    Install and configure devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_and_belongs_to_many :achievements
   has_many :posts
   has_one :bucket_list
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :bucket_lists
   resources :posts
   resources :achievements
