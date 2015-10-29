02ebb0c5a322ec12854f2a02af1aa15abe2e3521
Mikael Lisselgard <mikael.lisselgard@hotmail.se>
2015-10-29 12:38:18 +0000

    Set correct relations between all models

--- app/models/bucket_list.rb
+++ app/models/bucket_list.rb
@@ -1,3 +1,4 @@
 class BucketList < ActiveRecord::Base
   belongs_to :user
+  has_and_belongs_to_many :achievements
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_and_belongs_to_many :achievements
+  has_many :posts
+  has_one :bucket_list
 end
