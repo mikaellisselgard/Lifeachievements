9c2e43e1df3ee99c995f574becf810b8639f52b8
Mikael Lisselgard <mikael.lisselgard@hotmail.se>
2015-10-29 13:20:06 +0000

    Set relation and permit params in achievement and category controllers and forms

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -15,6 +15,7 @@ class AchievementsController < ApplicationController
   # GET /achievements/new
   def new
     @achievement = Achievement.new
+    @categories = Category.all
   end
 
   # GET /achievements/1/edit
@@ -69,6 +70,6 @@ class AchievementsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def achievement_params
-      params.require(:achievement).permit(:description, :score)
+      params.require(:achievement).permit(:description, :score, category_ids: [])
     end
 end
