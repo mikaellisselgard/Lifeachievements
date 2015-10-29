d7620a27c3129e8353d7a57faa502864365e9466
Mikael Lisselgard <mikael.lisselgard@hotmail.se>
2015-10-29 13:50:20 +0000

    Generate home controller with index method and set root to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :users
   resources :bucket_lists
   resources :posts
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
