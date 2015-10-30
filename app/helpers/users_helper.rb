0931f481508d37d45f806101afcac5a5d7608467
Mikael Lisselgard <mikael.lisselgard@hotmail.se>
2015-10-30 21:52:51 +0000

    Remove helper method to avoid bug on ajax delete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,3 @@
 module UsersHelper
 
-  def check_user_bucket_list
-    if current_user == User.find(params[:id])
-      return true # This returns true if the bucket list belongs to current user
-    end
-  end
 end
