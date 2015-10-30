fe10319aac0cf3fd84a2b53f457a3e075b13bc0b
Viktor <shorts@live.se>
2015-10-30 09:07:59 +0000

    Set achievement score and forbid users to complete achievement twice

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,25 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :achievement
+  before_create :check_achievement
+  after_create :update_achievement
+
+  def check_achievement
+    @achievement = self.achievement
+    @user = self.user
+    @achievement.posts.each do |post|
+      if post.user_id == @user.id
+        return false
+      end
+    end
+  end
+  
+  def update_achievement
+    @achievement = self.achievement
+    @user_id_length = @achievement.posts.length
+    @new_score = 105 - (@user_id_length * 5) 
+    @achievement.update_attributes(:score => @new_score)
+    @achievement.save!
+  end
   
 end
