6a66ad415544cf17d9fe815e57e52893d262e9f1
Mikael Lisselgard <mikael.lisselgard@hotmail.se>
2015-10-30 14:54:12 +0000

    Add bucket list to user page and able removal of achievements from bucket list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,15 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+
+    @user_bucket_list = @user.bucket_list.achievements
+
+    #Check if bucket list belongs to current user
+    @check_user_bucket_list = false
+    if current_user == @user
+      @check_user_bucket_list = true
+    end
+
   end
   
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+  def check_user_bucket_list
+    if current_user == User.find(params[:id])
+      return true # This returns true if the bucket list belongs to current user
+    end
+  end
 end
