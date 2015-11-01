01b15ef6620519d81367219a22ed83c517f64311
Viktor <shorts@live.se>
2015-11-01 11:38:53 +0000

    Highscore for users, sort by highest score

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,18 @@ class HomeController < ApplicationController
   end
   
   def highscore
+    @users = User.all
+    @user_stats = []
+    @users.each do |user|
+      @score = []
+      user.posts.each do |post|
+        @score.push(post.achievement.score)
+      end
+      @total_score = @score.inject(:+)
+      @achievements_earned = @score.length
+      @user_stats.push([user, @achievements_earned, @total_score])
+    end
+    @user_stats = @user_stats.sort_by{|k|k[2]}.reverse
   end
   
 end
