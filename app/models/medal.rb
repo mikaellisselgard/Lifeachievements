c1edf73ffb7e71ee3aca7a8fd5802516e8ed0f06
Viktor <shorts@live.se>
2015-11-01 09:17:41 +0000

    Score medal class method

--- app/models/medal.rb
+++ app/models/medal.rb
@@ -1,3 +1,36 @@
 class Medal < ActiveRecord::Base
   belongs_to :user
+  
+  def self.score_medal
+    # most gained score medal, weekly
+    @users = User.all
+    @user_with_highest_score = ''
+    @highest_score = 0
+    @users.each do |user|
+      @score = []
+      user.posts.each do |post|
+        #only fetching achievement gained last 7 days
+        if post.created_at + 7.days > Time.now
+          @score.push(post.achievement.score)
+        end
+      end
+      # checking if score is higher than previous highest
+      if @score.inject(:+) > @highest_score
+        @highest_score = @score.inject(:+)
+        @user_with_highest_score = user
+      end
+    end
+    # create the medal for the user
+    Medal.new({ :user_id => @user_with_highest_score.id, :type => 'score_medal', :image => 'score_medal.png' }).save
+  end
+  
+  
+  
+  private
+
+  #setting inheritance to nil, to use type as a column
+  def self.inheritance_column
+    nil
+  end
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :achievements
   has_many :posts
   has_one :bucket_list
+  has_many :medals
 
   after_create :set_bucket_list
 
