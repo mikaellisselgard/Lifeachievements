d909b7ee84d10f470710b0d6aca6a7e0f29bf5a5
Viktor <shorts@live.se>
2015-11-05 14:36:30 +0000

    Find user profiles by name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
   autocomplete :user, :name, full: true
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:edit, :update]
   
   def index
     @users = User.all
   end
   
   def show
-    @user = User.find(params[:id])
+    # user_find_by_id for updating and editing, find_by_name for finding users in search
+    if params[:id] == current_user.id.to_s
+      @user = User.find(params[:id])
+    else 
+      @user = User.find_by_name(params[:id])
+    end
     #@bucket_list = @user.bucket_list
   end
   
