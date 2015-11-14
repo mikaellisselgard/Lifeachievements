f14fa8c75b357817f2050837a690568cf7a14f04
Viktor <shorts@live.se>
2015-11-14 19:34:29 +0000

    Refactor bucket_list add and remove for less code in controller

--- app/models/bucket_list.rb
+++ app/models/bucket_list.rb
@@ -1,4 +1,15 @@
 class BucketList < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :achievements
+  
+  def add_achievement(achievement)
+    self.achievements << achievement
+    self.save!
+  end
+  
+  def remove_achievement(achievement)
+    self.achievements.delete(achievement)
+    self.save!
+  end
+  
 end
