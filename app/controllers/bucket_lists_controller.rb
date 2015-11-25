3ffb456011150e485cbe9af55585fa77c025db78
Viktor <shorts@live.se>
2015-11-25 06:56:59 +0000

    Refactor bucket-list show to use achievement partial

--- app/controllers/bucket_lists_controller.rb
+++ app/controllers/bucket_lists_controller.rb
@@ -2,7 +2,7 @@ class BucketListsController < ApplicationController
   
   def show
     @bucket_list = BucketList.find(params[:id])
-    @bucket_list_achievements = @bucket_list.achievements.reverse
+    @achievements = @bucket_list.achievements.reverse
   end
   
   def add_achievement
