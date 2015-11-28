9d7791ee73c8ddc12cb62e71b5a520c76346d235
Mikael Lisselgard <mikael.lisselgard@hotmail.se>
2015-11-28 14:09:12 +0000

    Helper method to check if achievement already exists in users bucket-list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,10 @@ module ApplicationHelper
     commenter = controller.controller_name.singularize
     comments_path(:commenter_type => commenter, :commenter_id => id)
   end
-  
+
+  def already_in_bucket_list(achievement)
+    if current_user.bucket_list.achievements.include?(achievement)
+    	true
+    end 
+  end
 end
