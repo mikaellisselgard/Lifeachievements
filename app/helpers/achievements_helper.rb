3418ad21394c120ecf9ced1ade1d08cb311bc0da
sholle <shorts@live.se>
2015-12-03 12:57:04 +0100

    Refactor already_in_bucket_list helper

--- app/helpers/achievements_helper.rb
+++ app/helpers/achievements_helper.rb
@@ -1,9 +1,9 @@
 module AchievementsHelper
 
 	def already_in_bucket_list(achievement)
-	    if current_user.bucket_list.achievements.include?(achievement)
-	    	true
-	    end 
-  	end
+    if user_signed_in?
+	     current_user.bucket_list.achievements.include?(achievement)
+    end
+  end
 
 end
