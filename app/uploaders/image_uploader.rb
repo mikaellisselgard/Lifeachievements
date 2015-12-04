2b4420db0517e7eb43a0a86ed75c1541f5d36505
sholle <shorts@live.se>
2015-12-04 10:09:36 +0100

    Save coords for uploaded images to post

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,6 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
   process :store_dimensions
   process :fix_exif_rotation
+  process :extract_geolocation
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -22,6 +23,38 @@ class ImageUploader < CarrierWave::Uploader::Base
     manipulate! do |img|
       img = img.auto_orient
     end
+  end
+  
+  def get_exif( name )
+    manipulate! do |img|
+      return img["EXIF:" + name]
+    end
+  end
+  
+  def extract_geolocation
+    img_lat = get_exif('GPSLatitude').split(', ') rescue nil
+    img_lng = get_exif('GPSLongitude').split(', ') rescue nil
+    
+    lat_ref = get_exif('GPSLatitudeRef') rescue nil
+    lng_ref = get_exif('GPSLongitudeRef') rescue nil
+    
+    return unless img_lat && img_lng && lat_ref && lng_ref
+    
+    # from [\"56/1\", \"10/1\", \"432/100\"] to 56.16786666666666
+    latitude = to_frac(img_lat[0]) + (to_frac(img_lat[1])/60) + (to_frac(img_lat[2])/3600)
+    longitude = to_frac(img_lng[0]) + (to_frac(img_lng[1])/60) + (to_frac(img_lng[2])/3600)
+    
+    latitude = latitude * -1 if lat_ref == 'S'  # (N is +, S is -)
+    longitude = longitude * -1 if lng_ref == 'W'   # (W is -, E is +)
+    
+    model.latitude = latitude
+    model.longitude = longitude
+  end
+  
+  def to_frac(strng)
+    numerator, denominator = strng.split('/').map(&:to_f)
+    denominator ||= 1
+    numerator/denominator
   end
   
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -56,17 +89,18 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
   private
-  
-
 
   def store_dimensions
     if file && model
-      @height = 280
-      @width_before, @height_before = `identify -format "%wx%h" #{file.path}`.split(/x/)
-      @dimension = @height_before.to_f / @width_before.to_f
-      model.width = @height
-      model.height = @height * @dimension
+      height = 280
+      width_before, height_before = `identify -format "%wx%h" #{file.path}`.split(/x/)
+      dimension = height_before.to_f / width_before.to_f
+      model.width = height
+      model.height = height * dimension
     end
   end
   
 end
+
+
+ 
