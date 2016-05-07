2023375b0a995e81385800ad9245fba4882bdd1c
sholle <shorts@live.se>
2016-05-07 16:33:49 +0200

    Refactor to use custom method to touch cause missing connection between post and comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,14 @@
 class Comment < ActiveRecord::Base
   belongs_to :imageble, polymorphic: true
   belongs_to :user
-  belongs_to :post, counter_cache: true, touch: true
+  belongs_to :post, counter_cache: true
   
   validates :comment, presence: true
+  
+  after_create :touch_post
+  
+  def touch_post
+    Post.find(self.imageable_id).touch
+  end
   
 end
