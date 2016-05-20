6c695abfb2c74d20f935d77ab7ad29934928aed2
Mikael Lisselgard <mikael.lisselgard@hotmail.se>
2016-05-20 11:21:15 +0000

    Add autocomplete with extra data

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -1,3 +1,3 @@
 class SearchResultsController < ApplicationController
-	autocomplete :search_result, :record_string, full: true, extra_data: [:record_type, :record_id]
+	autocomplete :search_result, :record_string, full: true, extra_data: [:record_type, :record_id, :record_image]
 end
